0cb30ca482dce2ff24e8758988c3f957bd74e25d
B-i-x <bix.romero@gmail.com>
2022-05-21 11:46:14 -0700

    starting the database

---
+++ config/local.py
@@ -0,0 +1,4 @@
+
+def Directory_database_path():
+
+    return r"C:\Users\alexr\Documents\Projects\Mathematical Politics\repository\build"
--- src/main.py
+++ src/main.py
@@ -1,8 +1,21 @@
 from src.classes.database_class import Database
+import config.local
 
 def main():
 
-    db = Database()
+    if not setup_database(): db = setup_database() 
+    
+
+def setup_database():
+
+    db = Database(config.local.Directory_database_path)
+
+    if db.connect(): pass
+    else:
+        print("database connection failed!")
+        return False
+
+    return db
 
 if __name__ == "__main__":
     main()
