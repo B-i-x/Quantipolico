7a120b75d111c7568983aabc6377438053e571b7
B-i-x <bix.romero@gmail.com>
2022-05-21 11:32:03 -0700

    were getting it going now

--- src/classes/database_class.py
+++ src/classes/database_class.py
@@ -1,1 +1,30 @@
-print('happy days')
+from socket import create_connection
+import sqlite3
+from sqlite3 import Error
+
+class Database():
+
+    def __init__(self, path) -> None:
+        self.path = path
+
+        self.conn = None
+        pass
+
+    def create_connection(self):
+        """ create a database connection to a SQLite database """
+        try:
+            conn = sqlite3.connect(self.path)
+            print('{} database connected'.format(self.path))
+        except Error as e:
+            print(e)
+        
+        return conn
+
+    def connect(self):
+        conn = self.create_connection()
+        
+        if conn.total_changes == 0: 
+            self.conn = conn
+            return True
+            
+        else: return False
---
+++ src/main.py
@@ -0,0 +1,9 @@
+from src.classes.database_class import Database
+
+def main():
+
+    db = Database()
+
+if __name__ == "__main__":
+    main()
+    
