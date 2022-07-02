849c6b5835cac322c84054ce99ae438632bcda22
B-i-x <bix.romero@gmail.com>
2022-07-02 10:39:33 -0700

    i dont want to refactor

--- src/article_scanner/next_page_layouts.py
+++ src/article_scanner/next_page_layouts.py
@@ -1,13 +1,55 @@
 class Next_Layout_Structure():
 
-    def __init__(self, next_button_xpath: str) -> None:
-        pass
+    def __init__(self, next_button_xpath: str, name: str, ids: int = None) -> None:
+
+        self.next_button_xpath = next_button_xpath
+
+        self.name = name
+        
+        self.specialized = False
+
+        if ids is not None:
+            self.ids = ids
+
+            self.specialized = True
 
 class A(Next_Layout_Structure):
 
     def __init__(self) -> None:
 
-        xpath = ""
+        name = "next_arrow"
 
-        super.__init__()
+        xpath = "//a[text()='next ›']"
+
+        super.__init__(xpath, name)
+
+class B(Next_Layout_Structure):
+
+    def __init__(self) -> None:
+
+        name = "double_arrow"
+
+        xpath = "//a[@title='Go to next page']"
+
+        super.__init__(xpath, name)
+
+class C(Next_Layout_Structure):
+
+    def __init__(self) -> None:
+
+        name = "Next_double_arrow"
+
+        xpath = "//a[text()='Next »']"
+
+        super.__init__(xpath, name)
+
+class D(Next_Layout_Structure):
+
+    def __init__(self) -> None:
+
+        name = "Next_arrow"
+
+        xpath = "//a[text()='Next >']"
+
+        super.__init__(xpath, name)
         
